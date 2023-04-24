7f3df29c4fddebc48f5a703ea8f3e0b72d1bbe51
Thomas Kintscher <thomas.kintscher.extern@univention.de>
2023-04-24 20:35:52 +0200

    feat(api): transparently proxy requests to UCS host

--- src/routers/passwordreset.py
+++ src/routers/passwordreset.py
@@ -1,4 +1,19 @@
-from fastapi import APIRouter, HTTPException
+import logging
+import os
+from typing import Any
+from urllib.parse import urljoin
+
+import aiohttp
+from fastapi import APIRouter, Request, Response
+
+
+# where to reach the UCS host
+ucs_host = os.environ["UCS_BASE_URL"]
+# where to reach the self-service module
+ucs_selfservice_prefix = urljoin(
+    ucs_host, "/univention/command/passwordreset/")
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="/univention/command/passwordreset",
@@ -7,10 +22,40 @@ router = APIRouter(
 
 
 @router.post("/get_user_attributes_values")
-async def get_user_attributes_values():
-    return HTTPException(status_code=418, detail="Test status code")
+async def get_user_attributes_values(request: Request, response: Response):
+    payload = await request.json()
+    cookies = request.cookies
+    headers = {"x-xsrf-protection": request.headers["x-xsrf-protection"]}
+
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        url = urljoin(ucs_selfservice_prefix,
+                      "get_user_attributes_descriptions")
+        async with session.post(url, headers=headers, json=payload) as ucs_response:
+            body = await ucs_response.json()
+            response.status_code = ucs_response.status
+            for key, value in ucs_response.cookies.items():
+                response.set_cookie(key, value)
+            return body
 
 
 @router.post("/get_user_attributes_descriptions")
-async def get_user_attributes_descriptions():
-    return HTTPException(status_code=418, detail="Test status code")
+async def get_user_attributes_descriptions(request: Request, response: Response) -> Any:
+    payload = await request.json()
+    cookies = request.cookies
+    headers = {"x-xsrf-protection": request.headers["x-xsrf-protection"]}
+
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        url = urljoin(ucs_selfservice_prefix,
+                      "get_user_attributes_descriptions")
+        async with session.post(url, headers=headers, json=payload) as ucs_response:
+            body = await ucs_response.json()
+            response.status_code = ucs_response.status
+            for key, value in ucs_response.cookies.items():
+                response.set_cookie(key, value)
+
+            for attribute in body.get("result", {}):
+                # TODO: let OPA make the decision...
+                attribute["editable"] = False
+                attribute["readonly"] = True
+
+            return body

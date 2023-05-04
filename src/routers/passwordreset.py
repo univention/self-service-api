47b6938e75bd7cf90f36e3c4b63b5b407e5c84dc
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-05-04 16:22:10 +0200

    feat(self-service-api): Add verbose logging around the request handling

--- src/routers/passwordreset.py
+++ src/routers/passwordreset.py
@@ -21,6 +21,10 @@ settings = Settings()
 logger = logging.getLogger(__name__)
 
 
+logger.info("Using ucs_host %s", settings.ucs_host)
+logger.info("Using ucs_selfservice_prefix %s", settings.ucs_selfservice_prefix)
+
+
 router = APIRouter(
     prefix="/univention/command/passwordreset",
     tags=["passwordreset", "univention", "umc"],
@@ -35,20 +39,27 @@ async def get_user_attributes_values(
     headers = {"x-xsrf-protection": request.headers.get("x-xsrf-protection")} \
         if request.headers.get("x-xsrf-protection") else {}
 
+    logger.debug("Request to get_user_attributes_values")
+
     async with aiohttp.ClientSession(cookies=cookies) as session:
         url = urljoin(settings.ucs_selfservice_prefix,
                       "get_user_attributes_values")
+        logger.debug("Forwarding to %s", url)
+
         async with session.post(url, headers=headers, json=payload) as ucs_response:
             body = await ucs_response.json()
+            logger.debug("Got response from selfservice: %s", body)
             response.status_code = ucs_response.status
             for key, value in ucs_response.cookies.items():
                 response.set_cookie(key, value)
 
+            logger.debug("Calling to OPA")
             res = await opa_client.check_policy(
                 policy="/v1/data/self_service/filters/user_data_attribute_values",
                 data=body.get("result", {}))
             body["result"] = res
 
+            logger.debug("Returning %s", body)
             return body
 
 
@@ -61,20 +72,28 @@ async def get_user_attributes_descriptions(
     headers = {"x-xsrf-protection": request.headers.get("x-xsrf-protection")} \
         if request.headers.get("x-xsrf-protection") else {}
 
+    logger.debug("Request to get_user_attributes_descriptions")
+
     async with aiohttp.ClientSession(cookies=cookies) as session:
         url = urljoin(settings.ucs_selfservice_prefix,
                       "get_user_attributes_descriptions")
+        logger.debug("Forwarding to %s", url)
+
         async with session.post(url, headers=headers, json=payload) as ucs_response:
             body = await ucs_response.json()
+            logger.debug("Got response from selfservice: %s", body)
             response.status_code = ucs_response.status
             for key, value in ucs_response.cookies.items():
                 response.set_cookie(key, value)
 
+            logger.debug("Calling to OPA")
             res = await opa_client.check_policy(
                 policy="/v1/data/self_service/filters/user_data_attribute_descriptions",
                 data=body.get("result", {}))
+            logger.debug("Got OPA result")
             body["result"] = res
 
+            logger.debug("Returning %s", body)
             return body
 
 
@@ -85,11 +104,16 @@ async def command(request: Request, response: Response, command) -> Any:
     headers = {"x-xsrf-protection": request.headers.get("x-xsrf-protection")} \
         if request.headers.get("x-xsrf-protection") else {}
 
+    logger.debug("Request to %s", command)
+
     async with aiohttp.ClientSession(cookies=cookies) as session:
         url = urljoin(settings.ucs_selfservice_prefix,
                       command)
+        logger.debug("Forwarding request to %s", url)
+
         async with session.post(url, headers=headers, json=payload) as ucs_response:
             body = await ucs_response.json()
+            logger.debug("Got response")
             response.status_code = ucs_response.status
             for key, value in ucs_response.cookies.items():
                 response.set_cookie(key, value)
